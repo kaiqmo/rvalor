8df669c89c73b65a88ddf4dc7d5a929f70bd0d69
kaiqmo <kaiqmo@gmail.com>
2020-03-16 20:56:58 -0300

    Updates

---
+++ precache-manifest.034d8316cebc10d9f8f32c6d4759840f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad53bf104658def7149574114a86b70",
+    "url": "/rvalor/index.html"
+  },
+  {
+    "revision": "02e088281107988168f5",
+    "url": "/rvalor/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c7c5870ebd616a9259f1",
+    "url": "/rvalor/static/css/main.915afd92.chunk.css"
+  },
+  {
+    "revision": "02e088281107988168f5",
+    "url": "/rvalor/static/js/2.a03c80a7.chunk.js"
+  },
+  {
+    "revision": "279b8325d7a78e7486d43155551b0b1d",
+    "url": "/rvalor/static/js/2.a03c80a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c5870ebd616a9259f1",
+    "url": "/rvalor/static/js/main.3b1e3281.chunk.js"
+  },
+  {
+    "revision": "4663ed3944718285beae",
+    "url": "/rvalor/static/js/runtime-main.7b252498.js"
+  },
+  {
+    "revision": "b1dfe40021620a12bd937c3d3c1ee86a",
+    "url": "/rvalor/static/media/real.b1dfe400.png"
+  }
+]);
